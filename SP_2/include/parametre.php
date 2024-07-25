546feceadb9e7882cec64e381010bc046d32d4c5
JorisLacassaigne <joris36.lacassaigne@gmail.com>
2024-07-25 09:44:02 +0200

    Maj des infos concernant la bdd pour le vps

--- SP_2/include/parametre.php
+++ SP_2/include/parametre.php
@@ -1,7 +1,7 @@
 <?php
 
 define('SERVEUR', 'localhost');
-define('BASE', 'seraphinparysfilrouge_2');
-define('NOM', 'root');
-define('PASSE', '');
+define('BASE', 'seraphinparysfilrouge_');
+define('NOM', 'adminsp');
+define('PASSE', 'AdminSeraphinParys');
 
