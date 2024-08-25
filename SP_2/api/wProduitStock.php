8cfeb04385ed22d0e4bfb2904a71223169846bb5
Camceleb <lilianjimenez@hotmail.fr>
2024-08-25 19:31:21 +0200

    Ajout command een BDD pratiquement terminé. Client entré en dur

--- SP_2/api/wProduitStock.php
+++ SP_2/api/wProduitStock.php
@@ -1,41 +1,29 @@
 <?php
+
 try {
-
-    // Interface de connexion avec Windev p
-    if (isset($_GET['wDemande']) && $_GET['wDemande'] == 'azerty2QWERTY' ){
-
-        // Définir les infos de cnx
-
+    if (isset($_GET['wDemande']) && $_GET['wDemande'] == 'getProduits') {
         define('SERVEUR', 'localhost');
-        define('BASE', 'seraphinparys');
+        define('BASE', 'seraphinparysfilrouge_2');
         define('NOM', 'root');
         define('PASSE', '');
 
         $cnx = new PDO('mysql:host=' . SERVEUR . ';dbname=' . BASE, NOM, PASSE, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-        $sql = "SELECT * FROM produit WHERE stock < 50";
+        $sql = "SELECT * FROM produit where quantite <= 50";
 
         $idRequete = $cnx->query($sql);
 
-        if (!$idRequete){
-            echo "Erreur : recupération de données impossible";
+        if (!$idRequete) {
+            echo "Erreur : recuperation de données impossible";
         }
 
-        while($row = $idRequete->fetch(PDO::FETCH_ASSOC)){
-
-            echo $row['reference'] . " ; " . $row['designation'] . " ; " . $row['quantite'] . " ; " . $row['descriptif'] . " ; " . $row['prix_unitaire_HT'] . " ; " . $row['stock'];
+        while ($row = $idRequete->fetch(PDO::FETCH_ASSOC)) {
+            echo $row['reference'] . ";" . $row['designation'] . ";" . $row['quantite'] . ";" . $row['descriptif'] . ";" . $row["prixUnitaireHT"] . ";" . $row["stock"] . ";" . $row["poidsPiece"] . " sep ";
         }
-
-
-
-    }else{
-        echo "ACCES INTERDIT";
+    } else {
+        echo "ACCES INTERDIT (texte vu par Lilian)";
     }
-
-
-}catch (PDOException $e ){
-
+} catch (PDOException $e) {
     echo "Echec de connexion : " . $e->getMessage();
-    exit;
-
+    exit();
 }
+?>
