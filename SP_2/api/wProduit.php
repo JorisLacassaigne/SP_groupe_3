f57a19c368fb53f8762d494b9d4c17c912101cf2
Camceleb <lilianjimenez@hotmail.fr>
2024-09-13 15:10:47 +0200

    API pour afficher les produits sur Windev

---
+++ SP_2/api/wProduit.php
@@ -0,0 +1,30 @@
+<?php
+
+try {
+    if (isset($_GET['wDemande']) && $_GET['wDemande'] == 'getProduits') {
+        define('SERVEUR', 'localhost');
+        define('BASE', 'seraphinparysfilrouge_2');
+        define('NOM', 'jlacassaigne36');
+        define('PASSE', 'Talwegfomec82.');
+
+
+        $cnx = new PDO('mysql:host=' . SERVEUR . ';dbname=' . BASE, NOM, PASSE, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $sql = "SELECT * FROM produit";
+
+        $idRequete = $cnx->query($sql);
+
+        if (!$idRequete) {
+            echo "Erreur : recuperation de données impossible";
+        }
+
+        while ($row = $idRequete->fetch(PDO::FETCH_ASSOC)) {
+            echo $row['reference'] . ";" . $row['designation'] . ";" . $row['quantite'] . ";" . $row['descriptif'] . ";" . $row["prixUnitaireHT"] . ";" . $row["stock"] . ";" . $row["poidsPiece"] . " sep ";
+        }
+    } else {
+        echo "ACCES INTERDIT";
+    }
+} catch (PDOException $e) {
+    echo "Echec de connexion : " . $e->getMessage();
+    exit();
+}
+?>
